3dcf678df2afd8466e742224254d0dd2d255ab14
Paul Higgs <paul_higgs@hotmail.com>
2017-03-17 07:56:48 +0800

    Initial Source

---
+++ GCF.cpp
@@ -0,0 +1,32 @@
+// GCF.cpp : Greatest Common Factor (using the Euclidian method
+//
+
+#include "stdafx.h"
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MAX(a,b) (((a)>(b))?(a):(b))
+
+int gcf(int divisor, int dividend)
+{
+	if (dividend % divisor == 0)
+		return divisor;
+	else return gcf(dividend % divisor, divisor);
+}
+
+
+
+void main(int argc, char *argv[])
+{
+	if (argc != 3) {
+		printf("usage: %s num1 num2\n", argv[0]);
+		return;
+	}
+	int a1 = atoi(argv[1]);
+	int a2 = atoi(argv[2]);
+	printf("GCF of %d and %d is %d\n", a1, a2, gcf(MIN(a1, a2), MAX(a1, a2)));
+}
